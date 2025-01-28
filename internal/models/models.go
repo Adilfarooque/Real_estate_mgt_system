0142c0a32ba5128d839fa331c2ba08bfe67d19e4
Adilfarooque <Adilfarq11@gmail.com>
2025-01-28 22:49:12 +0530

    Models Created

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,34 @@
 package models
+
+import "time"
+
+type User struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	Role      string    `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Property struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Price       int       `json:"price"`
+	Location    string    `json:"location"`
+	UserID      uint      `json:"user_id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type Booking struct {
+	ID         int       `json:"id"`
+	PropertyID int       `json:"property_id"`
+	UserID     int       `json:"user_id"`
+	CheckIn    time.Time `json:"check_in"`
+	CheckOut   time.Time `json:"check_out"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+}
