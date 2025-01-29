68cd68b4087050656b75cd83dc428dea73f268ff
Adilfarooque <Adilfarq11@gmail.com>
2025-01-29 15:10:34 +0530

    Updated

---
+++ go.mod
@@ -0,0 +1,3 @@
+module real_estate_mgt_system
+
+go 1.22.2
--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -1,1 +1,27 @@
 package repository
+
+import (
+	"real_estate_mgt_system/internal/models"
+
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	CreateUser(user models.User) error
+	GetUserByID(id models.User) (models.User, error)
+	GetUserByEmail(email models.User) (models.User, error)
+	UpdateUser(user models.User) error
+	DeleteUser(id uint) error
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db: db}
+}
+
+func (r *userRepository) CreateUser(user models.User)error{
+	return r.db.Create(&user).Error
+}
