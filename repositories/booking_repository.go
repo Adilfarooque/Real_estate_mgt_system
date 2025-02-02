9ba26e1448c416038fddfd39a19b58c2e4765bf7
Adilfarooque <Adilfarq11@gmail.com>
2025-02-02 13:53:20 +0530

    Updated

--- repositories/booking_repository.go
+++ repositories/booking_repository.go
@@ -1,12 +1,32 @@
 package repository
 
-import "real_estate_mgt_system/internal/models"
+import (
+	"real_estate_mgt_system/internal/models"
+
+	"gorm.io/gorm"
+)
 
 type BookingRepository interface {
 	CreateBooking(booking *models.Booking)
-	GetBookingByID(id uint)(*models.Booking,error)
-	GetBookingByUserID(userID uint)([]*models.Booking,error)
+	GetBookingByID(id uint) (*models.Booking, error)
+	GetBookingByUserID(userID uint) ([]*models.Booking, error)
 	UpdateBooking(booking *models.Booking)
 	DeleteBooking(id uint)
 }
 
+type bookingRepository struct {
+	db *gorm.DB
+}
+
+func NewBookingRepository(db *gorm.DB) BookingRepository {
+	return &bookingRepository{db: db}
+}
+
+func (r *bookingRepository) CreateBooking(booking *models.Booking) error {
+	return r.db.Create(booking).Error
+}
+
+func (r *bookingRepository) GetBookingByID(id uint) (*models.Booking,error){
+	var booking models.Booking
+	err := r.db.First(&booking, id).Error
+}
